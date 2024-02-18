b972024aab4a6fd11160eacaf775b58631fd0e45
Bouke Nijhuis <bouke.nijhuis@gmail.com>
2024-02-18 18:40:23 +0100

    Add minimal functionality + remove obsolete files

---
+++ src/main/kotlin/com/github/boukenijhuis/breakpointlogselection/BreakpointLogAction.kt
@@ -0,0 +1,49 @@
+package com.github.boukenijhuis.breakpointlogselection
+
+import com.intellij.openapi.actionSystem.ActionUpdateThread
+import com.intellij.openapi.actionSystem.AnAction
+import com.intellij.openapi.actionSystem.AnActionEvent
+import com.intellij.openapi.actionSystem.CommonDataKeys
+import com.intellij.xdebugger.XSourcePosition
+import com.intellij.xdebugger.breakpoints.SuspendPolicy
+import com.intellij.xdebugger.impl.XSourcePositionImpl
+import com.intellij.xdebugger.impl.breakpoints.XBreakpointUtil
+
+
+class BreakpointLogAction : AnAction() {
+
+    override fun getActionUpdateThread(): ActionUpdateThread {
+        return ActionUpdateThread.BGT
+    }
+
+    override fun actionPerformed(event: AnActionEvent) {
+        // get current project and file
+        val project = event.project ?: return
+        val currentFile = event.getData(CommonDataKeys.VIRTUAL_FILE) ?: return
+        val editor = event.getRequiredData(CommonDataKeys.EDITOR)
+
+        // determine the position variables
+        val offset = editor.caretModel.offset
+        val currentPosition = XSourcePositionImpl.createByOffset(currentFile, offset) as XSourcePosition
+        val nextLinePosition = XSourcePositionImpl.create(currentFile, currentPosition.line + 1)
+
+        // check the selection and determine the breakpoint position
+        val selectedText = editor.selectionModel.selectedText
+        var position = currentPosition
+        if (selectedText != null) {
+            position = nextLinePosition
+        }
+
+        // always toggle (even if there is no selection)
+        val breakpoint =
+            XBreakpointUtil.toggleLineBreakpoint(project, position, editor, false, false, true)
+
+        // update the breakpoint with the log expression
+        if (selectedText != null) {
+            breakpoint.then {
+                it.suspendPolicy = SuspendPolicy.NONE
+                it.logExpression = "\"$selectedText = [\" + $selectedText + \"]\""
+            }
+        }
+    }
+}
